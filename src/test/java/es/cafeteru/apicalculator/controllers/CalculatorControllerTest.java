a8632f89be21e958580c4d291e282ca74d5732ee
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2022-01-30 19:08:59 +0100

    Añade controlador y simplifica tests controlador

--- src/test/java/es/cafeteru/apicalculator/controllers/CalculatorControllerTest.java
+++ src/test/java/es/cafeteru/apicalculator/controllers/CalculatorControllerTest.java
@@ -9,6 +9,7 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
 
 import es.cafeteru.apicalculator.services.CalculatorService;
 import lombok.var;
@@ -29,21 +30,16 @@ class CalculatorControllerTest {
         var mockResultado = BigDecimal.valueOf(5);
         when(calculatorService.suma(any(), any())).thenReturn(mockResultado);
         var responseEntity = calculatorController.suma(BigDecimal.valueOf(2), BigDecimal.valueOf(3));
-        var statusCode = responseEntity.getStatusCode();
-        var resultado = responseEntity.getBody();
-        Assertions.assertTrue(statusCode.is2xxSuccessful());
-        Assertions.assertEquals(mockResultado, resultado);
+        compruebaRespuestaOK(responseEntity, mockResultado);
     }
+
 
     @Test
     void suma_con_valores_negativos() {
         var mockResultado = BigDecimal.valueOf(-5);
         when(calculatorService.suma(any(), any())).thenReturn(mockResultado);
         var responseEntity = calculatorController.suma(BigDecimal.valueOf(-2), BigDecimal.valueOf(-3));
-        var statusCode = responseEntity.getStatusCode();
-        var resultado = responseEntity.getBody();
-        Assertions.assertTrue(statusCode.is2xxSuccessful());
-        Assertions.assertEquals(mockResultado, resultado);
+        compruebaRespuestaOK(responseEntity, mockResultado);
     }
 
     @Test
@@ -51,16 +47,46 @@ class CalculatorControllerTest {
         var mockResultado = BigDecimal.valueOf(-1);
         when(calculatorService.suma(any(), any())).thenReturn(mockResultado);
         var responseEntity = calculatorController.suma(BigDecimal.valueOf(2), BigDecimal.valueOf(-3));
-        var statusCode = responseEntity.getStatusCode();
-        var resultado = responseEntity.getBody();
-        Assertions.assertTrue(statusCode.is2xxSuccessful());
-        Assertions.assertEquals(mockResultado, resultado);
+        compruebaRespuestaOK(responseEntity, mockResultado);
 
         mockResultado = BigDecimal.valueOf(1);
         when(calculatorService.suma(any(), any())).thenReturn(mockResultado);
         responseEntity = calculatorController.suma(BigDecimal.valueOf(-2), BigDecimal.valueOf(3));
-        statusCode = responseEntity.getStatusCode();
-        resultado = responseEntity.getBody();
+        compruebaRespuestaOK(responseEntity, mockResultado);
+    }
+
+    @Test
+    void resta_con_valores_positivos() {
+        var mockResultado = BigDecimal.valueOf(-1);
+        when(calculatorService.resta(any(), any())).thenReturn(mockResultado);
+        var responseEntity = calculatorController.resta(BigDecimal.valueOf(2), BigDecimal.valueOf(3));
+        compruebaRespuestaOK(responseEntity, mockResultado);
+    }
+
+    @Test
+    void resta_con_valores_negativos() {
+        var mockResultado = BigDecimal.valueOf(1);
+        when(calculatorService.resta(any(), any())).thenReturn(mockResultado);
+        var responseEntity = calculatorController.resta(BigDecimal.valueOf(-2), BigDecimal.valueOf(-3));
+        compruebaRespuestaOK(responseEntity, mockResultado);
+    }
+
+    @Test
+    void resta_con_valor_positivo_y_negativo() {
+        var mockResultado = BigDecimal.valueOf(5);
+        when(calculatorService.resta(any(), any())).thenReturn(mockResultado);
+        var responseEntity = calculatorController.resta(BigDecimal.valueOf(2), BigDecimal.valueOf(-3));
+        compruebaRespuestaOK(responseEntity, mockResultado);
+
+        mockResultado = BigDecimal.valueOf(-5);
+        when(calculatorService.resta(any(), any())).thenReturn(mockResultado);
+        responseEntity = calculatorController.resta(BigDecimal.valueOf(-2), BigDecimal.valueOf(3));
+        compruebaRespuestaOK(responseEntity, mockResultado);
+    }
+
+    private void compruebaRespuestaOK(ResponseEntity<BigDecimal> responseEntity, BigDecimal mockResultado) {
+        var statusCode = responseEntity.getStatusCode();
+        var resultado = responseEntity.getBody();
         Assertions.assertTrue(statusCode.is2xxSuccessful());
         Assertions.assertEquals(mockResultado, resultado);
     }
