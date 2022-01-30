170e4b91c2b74ce698c1a57df85d5ed86aadc797
Iván González Mahagamage <ivangonzalezmahagamage@gmail.com>
2022-01-30 17:07:06 +0100

    Añade configuración servidor REST y controlador

---
+++ src/main/java/es/cafeteru/apicalculator/controllers/CalculatorController.java
@@ -0,0 +1,10 @@
+package es.cafeteru.apicalculator.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class CalculatorController {
+}
